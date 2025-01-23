63e7a651719b38e63c0b79286a093f8ada02af6e
shaikh shahzade <shahzade10@gmail.com>
2025-01-23 21:54:50 +0530

    Client fix

--- Client/booking/src/app/app-routing.module.ts
+++ Client/booking/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
+import { MyBookingComponent } from './my-booking/my-booking.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'my-booking', component: MyBookingComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/booking/src/app/app.module.ts
+++ Client/booking/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductComponent } from './product/product.component';
+import { BookingComponent } from './booking/booking.component';
+import { MyBookingComponent } from './my-booking/my-booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductComponent,
+    BookingComponent,
+    MyBookingComponent
   ],
   imports: [
     BrowserModule,
